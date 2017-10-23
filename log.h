5c2e65866429072b6b4c67342e61203f5fa0fcae
asia <start2016@outlook.com>
2017-10-23 23:35:52 +0800

    the log color can be turned on or turned off

--- log.h
+++ log.h
@@ -21,8 +21,14 @@
  CYAN         36
  WHITE        37
 */
+
+/*log level for printing*/
 #define LOG_LEVEL INFO
 
+/*print different level log in different color*/
+#define COLOR_ON
+
+/*log level*/
 #define ERR  0
 #define WARN 1
 #define INFO 2
@@ -33,29 +39,44 @@ char levelStr[10][10] = {
     "INFO"
     };
 
-#define LOG(level, format, ...) \
-        do{\
-            if(level <= LOG_LEVEL){\
-            struct tm ptm = { 0 };\
-            char date[20] = {0};\
-            time_t Now =time(NULL);\
-            localtime_r(&Now, &ptm);\
-            strftime(date,20,"%x %X",&ptm);\
-            switch(level){\
-                    case ERR:\
-                        fprintf(stdout,"[%s %s %s: %d]\033[1;31m%s\033[0m:  "format"\n",\
-                             date, __FUNCTION__, __FILE__, __LINE__, levelStr[level], ##__VA_ARGS__);\
-                    break;\
-                    case WARN:\
-                        fprintf(stdout,"[%s %s %s: %d]\033[1;33m%s\033[0m:  "format"\n",\
-                        date, __FUNCTION__, __FILE__, __LINE__, levelStr[level], ##__VA_ARGS__);\
-                    break;\
-                    case INFO:\
-                        fprintf(stdout,"[%s %s %s: %d]\033[1;32m%s\033[0m:  "format"\n",\
-                        date, __FUNCTION__, __FILE__, __LINE__, levelStr[level], ##__VA_ARGS__);\
-                    break;\
+#ifdef COLOR_ON    
+    #define LOG(level, format, ...) \
+            do{\
+                if(level <= LOG_LEVEL){\
+                struct tm ptm = { 0 };\
+                char date[20] = {0};\
+                time_t Now =time(NULL);\
+                localtime_r(&Now, &ptm);\
+                strftime(date,20,"%x %X",&ptm);\
+                switch(level){\
+                        case ERR:\
+                            fprintf(stdout,"[%s %s %s: %d]\033[1;31m%s\033[0m:  "format"\n",\
+                                date, __FUNCTION__, __FILE__, __LINE__, levelStr[level], ##__VA_ARGS__);\
+                        break;\
+                        case WARN:\
+                            fprintf(stdout,"[%s %s %s: %d]\033[1;33m%s\033[0m:  "format"\n",\
+                            date, __FUNCTION__, __FILE__, __LINE__, levelStr[level], ##__VA_ARGS__);\
+                        break;\
+                        case INFO:\
+                            fprintf(stdout,"[%s %s %s: %d]\033[1;32m%s\033[0m:  "format"\n",\
+                            date, __FUNCTION__, __FILE__, __LINE__, levelStr[level], ##__VA_ARGS__);\
+                        break;\
+                    }\
                 }\
-            }\
-        }while(0)\
+            }while(0)
+#else  
+    #define LOG(level, format, ...) \
+    do{\
+        if(level <= LOG_LEVEL){\
+        struct tm ptm = { 0 };\
+        char date[20] = {0};\
+        time_t Now =time(NULL);\
+        localtime_r(&Now, &ptm);\
+        strftime(date,20,"%x %X",&ptm);\
+        fprintf(stdout,"[%s %s %s: %d]%s:  "format"\n",\
+                date, __FUNCTION__, __FILE__, __LINE__, levelStr[level], ##__VA_ARGS__);\
+        }\
+    }while(0)
+#endif
 
 #endif /* MYWORK_MYLOG_LOG_H_ */
